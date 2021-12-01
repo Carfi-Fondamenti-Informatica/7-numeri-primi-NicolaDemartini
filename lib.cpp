aa749c1d8bdaf9ececf6935e58902b90b2362a47
NicolaDemartini <91873902+NicolaDemartini@users.noreply.github.com>
2021-12-01 20:43:51 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+#include "lib.h"
+using namespace std;
+int main(){
+    int numero=0;
+    cin>>numero;
+    if(primo(2,numero)){
+        cout<<"numero primo"<<endl;
+    }
+    else {
+        cout<<"numero non primo"<<endl;
+    }
+    
+    return 0;
+}
